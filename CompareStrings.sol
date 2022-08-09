de4a72be80c6358751b7d92837679c4aa006b884
Cyber-Mitch <osawarumitchell@gmail.com>
2022-08-09 15:47:50 +0100

    Create CompareStrings.sol

---
+++ CompareStrings.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.15;
+
+library CompareStrings{
+
+    function compare(string memory text1, string memory text2) public pure returns(bool){
+    
+    return keccak256(abi.encodePacked(text1)) == keccak256(abi.encodePacked(text2)) ? true : false;
+
+    }
+}
